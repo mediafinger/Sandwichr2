7fa5c19940a914d970bd754ccf50249aef02e68a
Andreas Finger <webmaster@mediafinger.com>
2016-09-19 12:16:31 +0200

    Add many to many association between Sandwiches and Ingredients

--- app/models/sandwich.rb
+++ app/models/sandwich.rb
@@ -1,3 +1,6 @@
 class Sandwich < ApplicationRecord
+  has_many :sandwich_ingredients
+  has_many :ingredients, through: :sandwich_ingredients
+
   validates :name, presence: true, length: { minimum: 3, maximum: 255 }
 end
