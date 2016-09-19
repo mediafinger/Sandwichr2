2fc9c74ece4201c2ad1808a370c8950ee8167f53
Andreas Finger <webmaster@mediafinger.com>
2016-09-19 11:46:34 +0200

    Add Ingredient model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160919080931) do
+ActiveRecord::Schema.define(version: 20160919094529) do
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "calories"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "sandwiches", force: :cascade do |t|
     t.string   "name"
